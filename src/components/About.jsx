3fe7cb26cd52ae67eecd5db32b2fd79287ac7ddf
Zaxe <119933185+zaxe17@users.noreply.github.com>
2024-10-06 10:14:08 +0800

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,7 +17,7 @@ const About = () => {
 				<motion.div
 					whileInView={{ opacity: 1, x: 0 }}
 					initial={{ opacity: 0, x: -100 }}
-					transition={{ duration: 0.5 }}
+					transition={{ duration: 1.5 }}
 					className="w-full lg:w-1/2 lg:p-8">
 					<div className="flex items-center justify-center">
 						<img
@@ -31,7 +31,7 @@ const About = () => {
 				<motion.div
 					whileInView={{ opacity: 1, x: 0 }}
 					initial={{ opacity: 0, x: 100 }}
-					transition={{ duration: 0.5 }}
+					transition={{ duration: 1 }}
 					className="w-full lg:w-1/2">
 					<div className="flex justify-center lg:justify-start">
 						<p className="my-2 max-w-xl py-6">{ABOUT_TEXT}</p>
