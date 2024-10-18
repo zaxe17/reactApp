814f658a9702007d9cd5e125201221a0197b409c
Zaxe <jmjacolbiapogi17@gmail.com>
2024-10-18 21:51:56 +0800

    update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -37,16 +37,23 @@ const Projects = () => {
 							<h6 className="mb-2 font-semibold">
 								{project.title}
 							</h6>
-							<p className="mb-4 text-neutral-400">
+							<p className="mb-4 text-neutral-400 ">
 								{project.description}
 							</p>
-							{project.technologies.map((tech, index) => (
-								<span
-									key={index}
-									className="mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-900 transition-all ease-in-out duration-200 hover:text-purple-500 sm:hover:shadow-fuchsia-shadow">
-									{tech}
-								</span>
-							))}
+							<a
+								href={project.link}
+								className="block mb-2 text-purple-500 transition-all ease-in-out duration-200 hover:text-purple-300">
+								{project.link}
+							</a>
+							<div className="flex flex-wrap">
+								{project.technologies.map((tech, index) => (
+									<span
+										key={index}
+										className="mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-900 transition-all ease-in-out duration-200 hover:text-purple-500 sm:hover:shadow-fuchsia-shadow">
+										{tech}
+									</span>
+								))}
+							</div>
 						</motion.div>
 					</div>
 				))}
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,9 @@
+import { link } from "framer-motion/client";
 import project1 from "../assets/projects/project-1.png";
 import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.png";
 import project4 from "../assets/projects/project-4.jpg";
+import project5 from "../assets/projects/project-5.jpg";
 
 export const HERO_CONTENT = `I am a passionate full stack developer who loves building strong and scalable web applications. Working with front-end technologies like React and Next.js, as well as back-end technologies like Java, Node.js, MySQL, and MongoDB. My goal is to use my skills to create new solutions that help businesses grow and provide great experiences for users.`;
 
@@ -18,90 +20,101 @@ export const ABOUT_TEXT = `Welcome to my About Page! I'm a passionate and experi
                           presence and make your vision a reality!`;
 
 export const EXPERIENCES = [
-  {
-    year: "2024 - Present",
-    role: "Learning JavaScript",
-    company: "",
-    description: `Developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Node.js", "MongoDb"],
-  },
-  {
-    year: "2022 - 2024",
-    role: "Frontend Developer",
-    company: "",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Node.js", "Vue.js", "React.js"],
-  },
-  {
-    year: "2021 - 2022",
-    role: "Web Design",
-    company: "",
-    description: `Design and maintained web applications using Figma for desinging JavaScript, CSS, and HTML. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Figma", "HTML", "CSS", "JavaScript"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Backend Developer",
-    company: "",
-    description: `Learning Backend development handling server-side logic, database interactions, and application programming interfaces (APIs) to ensure seamless functionality and data management.`,
-    technologies: ["C/C++", "Java", "PHP", "XAMPP", "Bytehost", "MongoDb"],
-  },
+    {
+        year: "2024 - Present",
+        role: "Learning JavaScript",
+        company: "",
+        description: `Developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+        technologies: ["Javascript", "React.js", "Node.js", "MongoDb"],
+    },
+    {
+        year: "2022 - 2024",
+        role: "Frontend Developer",
+        company: "",
+        description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+        technologies: ["HTML", "CSS", "Node.js", "Vue.js", "React.js"],
+    },
+    {
+        year: "2021 - 2022",
+        role: "Web Design",
+        company: "",
+        description: `Design and maintained web applications using Figma for desinging JavaScript, CSS, and HTML. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+        technologies: ["Figma", "HTML", "CSS", "JavaScript"],
+    },
+    {
+        year: "2020 - 2021",
+        role: "Backend Developer",
+        company: "",
+        description: `Learning Backend development handling server-side logic, database interactions, and application programming interfaces (APIs) to ensure seamless functionality and data management.`,
+        technologies: ["C/C++", "Java", "PHP", "XAMPP", "Bytehost", "MongoDb"],
+    },
 ];
 
 export const PROJECTS = [
-  {
-    title: "Note Taking App",
-    image: project1,
-    description:
-      "A functional Note taking website to make everybody take note the important things.",
-    technologies: ["HTML", "CSS", "JavaScript", "PHP", "XAMPP"],
-  },
-  {
-    title: "Portfolio Website",
-    image: project2,
-    description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "JavaScript", "REACT", "Tailwind"],
-  },
-  {
-    title: "Blogging Platform",
-    image: project3,
-    description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "JavaScript"],
-  },
-  {
-    title: "Stream Lining Inventory",
-    image: project4,
-    description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "XAMPP", "PHP", "MySQL"],
-  },
+    {
+        title: "Note Taking App",
+        image: project1,
+        description:
+            "A functional Note taking website to make everybody take note the important things.",
+        link: "http://techtilanotess.22web.org/",
+        technologies: ["HTML", "CSS", "JavaScript", "PHP", "XAMPP"],
+    },
+    {
+        title: "Portfolio Website",
+        image: project2,
+        description:
+            "A personal portfolio website showcasing projects, skills, and contact information.",
+        technologies: ["HTML", "CSS", "JavaScript", "REACT", "Tailwind"],
+    },
+    {
+        title: "Blogging Platform",
+        image: project3,
+        description:
+            "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+        link: "https://janmarcjacolbia.netlify.app",
+        technologies: ["HTML", "CSS", "JavaScript"],
+    },
+    {
+        title: "Stream Lining Inventory",
+        image: project4,
+        description:
+            "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+        link: "http://streamlining.22web.org/",
+        technologies: ["HTML", "CSS", "XAMPP", "PHP", "MySQL"],
+    },
+    {
+        title: "Kawit, Cavite City",
+        image: project5,
+        description:
+            "This group project for our RIPH to show of beautiful places in Kawit, and also about history.",
+        link: "https://riph-kawit.netlify.app/",
+        technologies: ["HTML", "CSS", "JavaScript"],
+    },
 ];
 
 export const CONTACT = {
-  address: "Brgy. Sto. Cristo Bago Bantay, Quezon City ",
-  phoneNo: "+63 926 5424 417 ",
-  email: "jmjacolbiapogi17@gmail.com",
+    address: "Brgy. Sto. Cristo Bago Bantay, Quezon City ",
+    phoneNo: "+63 926 5424 417 ",
+    email: "jmjacolbiapogi17@gmail.com",
 };
 
 export const skills = [
-  {
-    category: "Front-end",
-    skills: [
-      { name: "HTML", level: 80 },
-      { name: "CSS", level: 90 },
-      { name: "JavaScript", level: 50 },
-      { name: "React.js", level: 30 },
-    ],
-  },
-  {
-    category: "Back-end",
-    skills: [
-      { name: "PHP", level: 75 },
-      { name: "Node.js", level: 54 },
-      { name: "Java", level: 48 },
-      { name: "C++", level: 40 },
-    ],
-  },
+    {
+        category: "Front-end",
+        skills: [
+            { name: "HTML", level: 80 },
+            { name: "CSS", level: 90 },
+            { name: "JavaScript", level: 50 },
+            { name: "React.js", level: 30 },
+        ],
+    },
+    {
+        category: "Back-end",
+        skills: [
+            { name: "PHP", level: 75 },
+            { name: "Node.js", level: 54 },
+            { name: "Java", level: 48 },
+            { name: "C++", level: 40 },
+        ],
+    },
 ];
