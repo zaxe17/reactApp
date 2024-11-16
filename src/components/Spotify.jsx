302e852a80c8dc9ed472f951e67a77f1bffb24e7
Zaxe <jmjacolbiapogi17@gmail.com>
2024-11-16 18:12:15 +0800

    update

--- src/components/Spotify.jsx
+++ src/components/Spotify.jsx
@@ -11,8 +11,8 @@ const Spotify = () => {
 				Spotify
 			</motion.h2>
 			<motion.div
-				whileInView={{ opacity: 1, x: 0 }}
-				initial={{ opacity: 0, x: -100 }}
+				whileInView={{ opacity: 1, scale: 1, y: 0 }}
+				initial={{ opacity: 0, scale: 0.80, y: 100 }}
 				transition={{ duration: 1.5 }}
 				className="flex items-center justify-center">
 				<iframe
