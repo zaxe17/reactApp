5e57590e855f3c486edefcad1a659eae5df3c332
Zaxe <jmjacolbiapogi17@gmail.com>
2025-01-27 14:14:22 +0800

    update

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -11,17 +11,33 @@ const Experience = () => {
 				className="my-20 text-center text-4xl">
 				Experience
 			</motion.h2>
-			<div className="">
+			<div className="relative">
+				<motion.div
+					className="absolute border-l-2 border-purple-700 -left-4 lg:left-1/3 transform -translate-x-1/2"
+					whileInView={{
+						opacity: 1,
+						height: "100%",
+					}}
+					initial={{
+						opacity: 1,
+						height: 0,
+					}}
+					transition={{ duration: 5 }}></motion.div>
 				{EXPERIENCES.map((experience, index) => (
 					<div
 						key={index}
-						className="mb-8 flex flex-wrap lg:justify-center">
+						className="mb-8 flex flex-wrap lg:justify-center relative">
+						<motion.div
+							className="absolute border-2 border-black top-0 -left-4 lg:left-1/3 transform -translate-x-1/2 h-4 w-4 rounded-full bg-purple-700"
+							whileInView={{ opacity: 1 }}
+							initial={{ opacity: 0 }}
+							transition={{ duration: 1 }}></motion.div>
 						<motion.div
 							whileInView={{ opacity: 1, x: 0 }}
 							initial={{ opacity: 0, x: -100 }}
 							transition={{ duration: 1 }}
 							className="w-full lg:w-1/4">
-							<p className="mb-2 text-sm text-neutral-400 ">
+							<p className="mb-2 text-sm text-neutral-400">
 								{experience.year}
 							</p>
 						</motion.div>
@@ -29,7 +45,7 @@ const Experience = () => {
 							whileInView={{ opacity: 1, x: 0 }}
 							initial={{ opacity: 0, x: 100 }}
 							transition={{ duration: 1.5 }}
-							className="w-full max-w-xl lg:w-3/4">
+							className="relative w-full max-w-xl lg:w-3/4">
 							<h6 className="mb-2 font-semibold ">
 								{experience.role}
 								<span className="text-sm text-purple-100">
