006dd34fdc4ab398b5c7c330f88b6d47d077ff23
Zaxe <jmjacolbiapogi17@gmail.com>
2025-04-11 16:53:52 +0800

    update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -37,8 +37,8 @@ const Projects = () => {
 							whileInView={{ opacity: 1, x: 0 }}
 							initial={{ opacity: 0, x: -100 }}
 							transition={{ duration: 1 }}
-							className="w-full lg:w-1/4 flex justify-center">
-							<div className="w-full h-[200px] relative overflow-hidden rounded-xl mb-6">
+							className="w-full lg:w-1/4 flex justify-center mx-24">
+							<div className="w-fit h-[200px] relative overflow-hidden rounded-xl mb-6">
 								<motion.img
 									key={current[index]}
 									src={project.image[current[index]]}
