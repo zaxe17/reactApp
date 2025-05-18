2a3ffc41cb14a1e7921a2c3282d5b67e2d827d95
Zaxe <jmjacolbiapogi17@gmail.com>
2025-05-18 22:45:19 +0800

    update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -71,7 +71,7 @@ const Projects = () => {
 								href={project.link}
 								target="_blank"
 								rel="noopener noreferrer"
-								className="block mb-2 text-purple-500 transition-all ease-in-out duration-200 hover:text-purple-300">
+								className="block mb-2 text-purple-500 transition-all ease-in-out duration-200 hover:text-purple-300 w-fit">
 								{project.link}
 							</a>
 							<div className="flex flex-wrap">
