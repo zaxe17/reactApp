e4eb0313467cb4afc8ebea391295ec66cf32dfda
Zaxe <jmjacolbiapogi17@gmail.com>
2026-02-08 15:08:41 +0800

    colors

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -24,16 +24,17 @@ const Experience = () => {
 					className="absolute border-l-2 border-purple-700 -left-4 lg:left-1/3 transform -translate-x-1/2 h-full"
 					whileInView={{ opacity: 1 }}
 					initial={{ opacity: 0 }}
-					transition={{ duration: 1 }}></motion.div>
+					transition={{ duration: 1 }} />
 				{EXPERIENCES.map((experience, index) => (
 					<div
 						key={index}
-						className="mb-8 flex flex-wrap lg:justify-center relative">
+						className="group mb-8 flex flex-wrap lg:justify-center relative">
 						<motion.div
-							className="absolute border-2 border-black my-5 -left-4 lg:left-1/3 transform -translate-x-1/2 h-4 w-4 rounded-full bg-purple-700"
+							className="absolute my-5 -left-4 lg:left-1/3 transform -translate-x-1/2 h-4 w-4 rounded-full bg-purple-700/40 backdrop-blur-md border border-white/20 shadow-lg shadow-purple-900/30 group-hover:bg-white/20 group-hover:border-white/40 group-hover:shadow-fuchsia-500/40 transition-all ease-in-out duration-300"
 							whileInView={{ opacity: 1 }}
 							initial={{ opacity: 0 }}
-							transition={{ duration: 1 }}></motion.div>
+							transition={{ duration: 1 }}
+						/>
 						<motion.div
 							whileInView={{ opacity: 1, x: 0 }}
 							initial={{ opacity: 0, x: -100 }}
@@ -48,20 +49,20 @@ const Experience = () => {
 							initial={{ opacity: 0, x: 100 }}
 							transition={{ duration: 1.5 }}
 							className="relative w-full max-w-xl lg:w-3/4">
-							<h6 className="mb-2 font-semibold ">
+							<h1 className="mb-2 font-semibold text-xl">
 								{experience.role}
 								<span className="text-sm text-purple-100">
 									{experience.company}
 								</span>
-							</h6>
-							<p className="mb-4 text-neutral-400">
+							</h1>
+							<p className="mb-4 text-neutral-400 transition-all duration-300 ease-in-out lg:group-hover:text-neutral-200">
 								{experience.description}
 							</p>
 							<div className="flex flex-wrap select-none">
 								{experience.technologies.map((tech, index) => (
 									<span
 										key={index}
-										className="mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800 transition-all ease-in-out duration-200 hover:text-purple-400 sm:hover:shadow-fuchsia-shadow">
+										className="mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-900 transition-all ease-in-out duration-300 hover:text-fuchsia-500 hover:bg-fuchsia-500/10 hover:shadow-fuchsia-shadow lg:group-hover:text-fuchsia-500 lg:group-hover:bg-fuchsia-500/10 lg:group-hover:shadow-fuchsia-shadow">
 										{tech}
 									</span>
 								))}
